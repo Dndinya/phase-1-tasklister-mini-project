ce10381f81858328fc284635b0a912908be06147
Derrick <derrick.ndinya@student.moringaschool.com>
2024-01-04 17:24:27 +0300

    Completed assignment

--- src/index.js
+++ src/index.js
@@ -1,3 +1,23 @@
 document.addEventListener("DOMContentLoaded", () => {
-  // your code here
+  let form = document.querySelector('form')
+  form.addEventListener('submit', (e) =>{
+    e.preventDefault()
+    takeTask(e.target.querySelector('#new-task-description').value)
+    form.reset()
+  })
 });
+
+function takeTask(todo){
+  let li = document.createElement('li')
+  let btn = document.createElement('button')
+  btn.addEventListener('click',deleteTask)
+  li.textContent =  `${todo}  `
+  btn.textContent = 'x'
+  li.appendChild(btn)
+  document.querySelector('#tasks').appendChild(li)
+  console.log(li)
+}
+
+function deleteTask(e){
+  e.target.parentNode.remove()
+}
